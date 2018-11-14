1db33834a9ee535a906bef05dfd155926e45a90a
pongtawan25 <pongtawan2541@gmail.com>
2018-11-14 22:29:53 +0700

    สร้าง loginservlet,แก้ index

--- RoyalStreetShoes/src/java/rss/servlet/LoginServlet.java
+++ RoyalStreetShoes/src/java/rss/servlet/LoginServlet.java
@@ -30,18 +30,9 @@ public class LoginServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(username != null && username.trim().length() > 0 && password != null && password.trim().length() > 0){
         }
     }
 
